6213a0f09a8c4a002ef3a0dc29835e84cc7c2ce2
Gonzalo <gonzalobggomez@gmail.com>
2026-04-29 18:45:51 +0200

    Guardar estadisticas hecho

--- METRICGOAL/Servidor/Controlador/jugador_controller.py
+++ METRICGOAL/Servidor/Controlador/jugador_controller.py
@@ -1,5 +1,6 @@
 from Modelo.Canterano import Canterano
 from Modelo.database import ejecutar_consulta
+from Modelo.EstadisticasTemporada import EstadisticasTemporada
 
 # Le cambiamos el nombre a la función para que sea único
 def gestionar_registro_canterano(datos_validados, id_equipo):
@@ -17,19 +18,44 @@ def gestionar_registro_canterano(datos_validados, id_equipo):
         return {"status": "error", "message": str(e)}
 
 def listar_jugadores_logic(id_equipo):
-    # Aseguramos creación por si acaso es la primera vez que se consulta
     ejecutar_consulta("""
-        CREATE TABLE IF NOT EXISTS Jugadores (
-            nombre TEXT,
-            apellido TEXT,
+        CREATE TABLE IF NOT EXISTS jugadores (
+            id_jugador INTEGER DEFAULT nextval('seq_jugadores_id') PRIMARY KEY,
+            id_equipo INTEGER,
+            nombre VARCHAR,
+            apellidos VARCHAR,
             edad INTEGER,
-            posicion TEXT
+            posicion VARCHAR
         )
     """)
     
-    query = "SELECT nombre, apellidos, posicion FROM jugadores WHERE id_equipo = ?"
+    query = "SELECT id_jugador, nombre, apellidos, posicion FROM jugadores WHERE id_equipo = ?"
     df = ejecutar_consulta(query, (id_equipo,))
     
     if df is not None and not df.empty:
         return df.to_dict(orient='records')
-    return []       
+    return []
+
+def obtener_stats_jugador_temporada(id_jugador, temporada):
+    query = """
+        SELECT goles, asistencias, tarj_amarillas, tarj_rojas, 
+               partidos_jugados, minutos_jugados, pases_clave 
+        FROM estadisticas_temporada 
+        WHERE id_jugador = ? AND temporada = ?
+    """
+    df = ejecutar_consulta(query, (id_jugador, temporada))
+    
+    if df is not None and not df.empty:
+        return df.to_dict(orient='records')[0] # Devolvemos la primera fila encontrada
+    return None # Si no hay nada, devolvemos None
+
+
+def gestionar_registro_stats(datos_validados, id_jugador):
+    try:
+        EstadisticasTemporada.guardar_estadisticas(id_jugador, datos_validados.dict())
+        return {"status": "success"}
+    except Exception as e:
+        print(f"Error en controlador stats: {e}")
+        return {"status": "error", "message": str(e)}
+
+    
